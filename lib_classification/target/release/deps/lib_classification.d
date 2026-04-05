16acc3c6dda924c460f38ac6a2f8069e9a08284a
TT7724 <theotes@hotmail.fr>
2026-04-05 11:55:21 +0200

    lib_rust
    - lib_rust sur l'entrainements
    - test python de la lib
    - début des cas_de_test

---
+++ lib_classification/target/release/deps/lib_classification.d
@@ -0,0 +1,5 @@
+C:\Users\theot\Documents\rust_prog\lib_classification\target\release\deps\lib_classification.d: src\lib.rs
+
+C:\Users\theot\Documents\rust_prog\lib_classification\target\release\deps\lib_classification.dll: src\lib.rs
+
+src\lib.rs:
