90d559393a8ddc15d5715fbb41612915b3ae6ecf
Byakoren <thomas.gouez-hoarau@live.fr>
2026-04-06 19:43:05 +0200

    feat/test-data-set: add linear and mlp models for first dataset tests

--- lib_classification/target/debug/deps/lib_classification-92fcbc91806f9c39.d
+++
@@ -1,5 +0,0 @@
-C:\Users\theot\Documents\rust_prog\lib_classification\target\debug\deps\lib_classification-92fcbc91806f9c39.d: src\lib.rs
-
-C:\Users\theot\Documents\rust_prog\lib_classification\target\debug\deps\liblib_classification-92fcbc91806f9c39.rmeta: src\lib.rs
-
-src\lib.rs:
--- lib_classification/target/release/deps/cpufeatures-6898f0e36e4cac47.d
+++
@@ -1,9 +0,0 @@
-C:\Users\theot\Documents\rust_prog\lib_classification\target\release\deps\cpufeatures-6898f0e36e4cac47.d: C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\lib.rs C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\x86.rs C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\../README.md
-
-C:\Users\theot\Documents\rust_prog\lib_classification\target\release\deps\libcpufeatures-6898f0e36e4cac47.rlib: C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\lib.rs C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\x86.rs C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\../README.md
-
-C:\Users\theot\Documents\rust_prog\lib_classification\target\release\deps\libcpufeatures-6898f0e36e4cac47.rmeta: C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\lib.rs C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\x86.rs C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\../README.md
-
-C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\lib.rs:
-C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\x86.rs:
-C:\Users\theot\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cpufeatures-0.3.0\src\../README.md:
