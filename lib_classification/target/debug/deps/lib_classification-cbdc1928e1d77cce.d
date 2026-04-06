5b8b5c26f413a36cc3249afa1c6480f8e53fe5cc
Byakoren <thomas.gouez-hoarau@live.fr>
2026-04-06 19:43:05 +0200

    feat/test-data-set: add linear and mlp models for first dataset tests

--- lib_classification/target/debug/deps/lib_classification-cbdc1928e1d77cce.d
+++
@@ -1,5 +0,0 @@
-C:\Users\theot\Documents\rust_prog\lib_classification\target\debug\deps\lib_classification-cbdc1928e1d77cce.d: src\lib.rs
-
-C:\Users\theot\Documents\rust_prog\lib_classification\target\debug\deps\liblib_classification-cbdc1928e1d77cce.rmeta: src\lib.rs
-
-src\lib.rs:
--- lib_classification/target/release/lib_classification.d
+++
@@ -1,1 +0,0 @@
-C:\Users\theot\Documents\rust_prog\lib_classification\target\release\lib_classification.dll: C:\Users\theot\Documents\rust_prog\lib_classification\src\lib.rs
