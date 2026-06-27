defc43a723b2e4a6d1c990f54238d60e36d1b911
StarNova <tanguy.collet2712@gmail.com>
2026-06-27 19:00:28 +0200

    cleared some stuff

--- lib_classification/src/lib.rs
+++ lib_classification/src/lib.rs
@@ -2,10 +2,10 @@ mod linear;
 mod linear_dataset;
 mod mlp;
 mod mlp_ffi;
-mod svm;
-mod svm_ffi;
+// mod svm;
+// mod svm_ffi;
 
 pub use linear::*;
 pub use linear_dataset::*;
 pub use mlp_ffi::*;
-pub use svm_ffi::*;
+// pub use svm_ffi::*;
