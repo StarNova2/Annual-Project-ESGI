df73f0affdac3dc6bfe8fc3cde00bba2bc0deca6
StarNova <tanguy.collet2712@gmail.com>
2026-07-01 11:41:19 +0200

    split the notebooks (one for each model) + cleaned some stuff

--- lib_classification/src/lib.rs
+++ lib_classification/src/lib.rs
@@ -4,14 +4,14 @@ mod linear;
 mod linear_dataset;
 mod mlp;
 mod mlp_ffi;
-mod RBF;
+mod rbf;
+mod rbf_dataset;
 
-mod RBF_database;
 // mod svm;
 // mod svm_ffi;
 
 pub use linear::*;
 pub use linear_dataset::*;
 pub use mlp_ffi::*;
-pub use RBF::*;
+pub use rbf::*;
 // pub use svm_ffi::*;
